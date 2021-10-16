5dc42d24d1ddd16da5c11a6601929f195ee02571
Richard <garric@okki.hu>
2021-10-16 10:26:25 +0200

    readback tests

--- picodb_test.go
+++ picodb_test.go
@@ -60,24 +60,61 @@ func Test_New(t *testing.T) {
 
 }
 
-func Test_Write(t *testing.T) {
+func Test_Readback(t *testing.T) {
 
 	path, err := os.MkdirTemp(os.TempDir(), "pdb")
 	require.NoError(t, err)
 	defer os.RemoveAll(path)
 
-	dir := path + "/write"
-	p, err := New(&PicoDbOptions{
-		RootPath: dir,
+	dir := path + "/readback"
+
+	t.Run("standard storage", func(t *testing.T) {
+		p, err := New(&PicoDbOptions{
+			RootPath: dir,
+		})
+		require.NoError(t, err)
+
+		data := testData{
+			Num:  5,
+			Str:  "test",
+			Flag: true,
+		}
+
+		assertReadback(t, p, data)
 	})
+
+	t.Run("compressed storage", func(t *testing.T) {
+		p, err := New(&PicoDbOptions{
+			RootPath:    dir,
+			Compression: true,
+		})
+		require.NoError(t, err)
+
+		data := testData{
+			Num:  5,
+			Str:  "test",
+			Flag: true,
+		}
+
+		assertReadback(t, p, data)
+	})
+
+}
+
+func assertReadback(t *testing.T, p *PicoDb, data interface{}) {
+	t.Helper()
+	err := p.Write("key", data)
 	require.NoError(t, err)
 
-	data := struct {
-		Str string
-		Num int
-	}{"test", 5}
+	var actual testData
+	err = p.Read("key", &actual)
+	require.NoError(t, err)
 
-	err = p.Write("key", data)
-	assert.NoError(t, err)
+	assert.Equal(t, data, actual)
+}
 
+type testData struct {
+	Num  int
+	Str  string
+	Flag bool
 }
