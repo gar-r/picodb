2df5835e1ff74428b0511eec65435a5f30a0e4a3
Richard <garric@okki.hu>
2021-10-19 22:11:30 +0200

    options tests

--- options.go
+++ options.go
@@ -8,6 +8,7 @@ type PicoDbOptions struct {
 	RootDir     string      // root directory
 	Compression bool        // enable compression at rest
 	Caching     bool        // enable in-memory cache
+	Locking     bool        // enable locking for write operations
 	FileMode    os.FileMode // file mode used to create files
 	DirMode     os.FileMode // file mode used to create directories
 }
@@ -21,4 +22,29 @@ func Defaults() *PicoDbOptions {
 		FileMode:    0644,
 		DirMode:     0744,
 	}
+}
+
+func (p *PicoDbOptions) WithCaching() *PicoDbOptions {
+	p.Caching = true
+	return p
+}
+
+func (p *PicoDbOptions) WithCompression() *PicoDbOptions {
+	p.Compression = true
+	return p
+}
+
+func (p *PicoDbOptions) WithLocking() *PicoDbOptions {
+	p.Locking = true
+	return p
+}
+
+func (p *PicoDbOptions) WithFileMode(mode os.FileMode) *PicoDbOptions {
+	p.FileMode = mode
+	return p
+}
+
+func (p *PicoDbOptions) WithDirMode(mode os.FileMode) *PicoDbOptions {
+	p.DirMode = mode
+	return p
 }
---
+++ options_test.go
@@ -0,0 +1,29 @@
+package picodb
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Defaults(t *testing.T) {
+	opt := Defaults()
+	assert.NotNil(t, opt)
+	assert.NotEmpty(t, opt.RootDir)
+}
+
+func Test_Builders(t *testing.T) {
+	opt := Defaults().
+		WithCaching().
+		WithCompression().
+		WithLocking().
+		WithFileMode(0666).
+		WithDirMode(0777)
+
+	assert.True(t, opt.Caching)
+	assert.True(t, opt.Compression)
+	assert.True(t, opt.Locking)
+	assert.Equal(t, os.FileMode(0666), opt.FileMode)
+	assert.Equal(t, os.FileMode(0777), opt.DirMode)
+}
